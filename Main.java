5b2a8562d9e2e379d2c551fa7ffdcc443e8e4beb
TOMHINC <74766450+TOMHINC@users.noreply.github.com>
2020-11-20 11:14:19 +0100

    Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+package ćwiczenia;
+
+public class Main {
+
+    public static void main(String[] args){
+        Human tomek = new Human();
+        Human kacper = new Human();
+        Human kamil = new Human ();
+
+        tomek.color_wlosow = "blond";
+        tomek.czy_lysy = false;
+        tomek.plec = 'm';
+        tomek.wzrost = 185;
+
+        kacper.color_wlosow = "czarne";
+        kacper.plec = 'm';
+        kacper.wiek = 22;
+        kacper.rozmiar_buta = 44;
+
+
+    }
+}
