9e42a192283299e6690da6381c0c59fb162dc777
TOMHINC <74766450+TOMHINC@users.noreply.github.com>
2020-11-20 11:14:00 +0100

    Human.java

---
+++ Human.java
@@ -0,0 +1,11 @@
+package ćwiczenia;
+
+public class Human {
+    double wzrost;
+    char plec; // m - mezczyzna // c - cos innego
+    double rozmiar_buta;
+    boolean czy_lysy;
+    String color_wlosow;
+    int wiek;
+
+}
